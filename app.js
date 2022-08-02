ac3641678bda6fb349100ff9fe5331ee43443309
Dadadon <davianberoni@yahoo.com>
2022-08-02 03:59:26 -0400

    add deleteBook, addBook and initialLoad functions

--- app.js
+++ app.js
@@ -1,31 +1,65 @@
-const books = [];
 const template = document.createElement("template");
 const form = document.forms["books-form"];
+var oldBooks = JSON.parse(localStorage.getItem("booksArray")) || [];
+
 function grab(e) {
   return document.getElementById(e);
 }
 function book(title, author) {
-    this.title = title;
-    this.author = author;
+  this.key = oldBooks.length;
+  this.title = title;
+  this.author = author;
 }
 
+function deleteBook(index) {
+  oldBooks.splice(index, 1);
+  localStorage.setItem("booksArray", JSON.stringify(oldBooks));
+  grab("books-container").innerHTML = "";
+  let c = 0;
+  oldBooks.forEach(function (bookItem) {
+    oldBooks[c].key = c;
+    template.innerHTML = `<li>
+                <h5>${bookItem.title}</h5>
+                <h5>${bookItem.author}</h5>
+                <button onclick="deleteBook(${c})">Remove</button>
+            </li>`;
+    console.log(oldBooks);
+      const far = template.content.firstElementChild;
+    grab("books-container").appendChild(far);
+    c++;
+  });
+}
 function addBook() {
-  console.log(book(form.booktitle, form.bookauthor));
-  //books.push(book(form.title, form.author));
-  template.innerHTML = "";
-  // books.forEach((el, index) => {
-  //   template.innerHTML = `<li>
-  //               <h5>${el.title}</h5>
-  //               <h5>${el.author}</h5>
-  //               <button onclick="deleteBook(${index})"></button>
-  //           </li>`;
-  // });
-  // const far = template.content.firstElementChild;
-  // grab("books-container").appendChild(far);
+  const title = form.booktitle;
+  const author = form.bookauthor;
+  var bookItem = new book(title.value, author.value);
+  oldBooks.push(bookItem);
+
+  localStorage.setItem("booksArray", JSON.stringify(oldBooks));
+
+  template.innerHTML = `<li>
+                <h5>${bookItem.title}</h5>
+                <h5>${bookItem.author}</h5>
+                <button onclick="deleteBook(${bookItem.key})">Remove</button>
+            </li>`;
+  console.log(oldBooks);
+  const far = template.content.firstElementChild;
+  grab("books-container").appendChild(far);
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addBook();
-});
-
+function initialLoad() {
+  let c = 0;
+  oldBooks.forEach(function (bookItem) {
+    oldBooks[c].key = c;
+    template.innerHTML = `<li>
+                <h5>${bookItem.title}</h5>
+                <h5>${bookItem.author}</h5>
+                <button onclick="deleteBook(${c})">Remove</button>
+            </li>`;
+    console.log(oldBooks);
+    const far = template.content.firstElementChild;
+    grab("books-container").appendChild(far);
+    c++;
+  });
+}
+initialLoad();
