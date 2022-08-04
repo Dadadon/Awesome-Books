678c10a6d2770e00e81da2b4c2207fd3e71d0435
Dadadon <davianberoni@yahoo.com>
2022-08-04 06:32:11 -0400

    revert changes from merged branch

--- app.js
+++ app.js
@@ -1,64 +1,45 @@
-const template = document.createElement('template');
-const form = document.forms['books-form'];
-const oldBooks = JSON.parse(localStorage.getItem('booksArray')) || [];
+const template = document.createElement("template");
+const form = document.forms["books-form"];
+const oldBooks = JSON.parse(localStorage.getItem("booksArray")) || [];
 
 function grab(e) {
   return document.getElementById(e);
 }
-function Book(title, author) {
-  this.key = oldBooks.length;
-  this.title = title;
-  this.author = author;
-}
 
-function deleteBook(index) {
-  window.location.reload();
-  oldBooks.splice(index, 1);
-  localStorage.setItem('booksArray', JSON.stringify(oldBooks));
-  grab('books-container').innerHTML = '';
-  let c = 0;
-  oldBooks.forEach((bookItem) => {
-    oldBooks[c].key = c;
-    template.innerHTML = `
-                <h5>${bookItem.title}</h5>
-                <h5>${bookItem.author}</h5>
-                <button onclick='deleteBook(${c})'>Remove</button>`;
-    const far = template.content.firstElementChild;
-    grab('books-container').appendChild(far);
-    c += c + 1;
-  });
-}
-function addBook() {
-  window.location.reload();
-  const title = form.booktitle;
-  const author = form.bookauthor;
-  const bookItem = new Book(title.value, author.value);
-  oldBooks.push(bookItem);
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-  localStorage.setItem('booksArray', JSON.stringify(oldBooks));
+  deleteBook(index) {
+    oldBooks.splice(index, 1);
+    localStorage.setItem("booksArray", JSON.stringify(oldBooks));
+    grab("books-container").innerHTML = "";
+    initialLoad();
+  }
+  addBook() {
+    const title = form.booktitle;
+    const author = form.bookauthor;
+    const bookItem = new Book(title.value, author.value);
+    oldBooks.push(bookItem);
 
-  template.innerHTML = `<li>
-                <h5>${bookItem.title}</h5>
-                <h5>${bookItem.author}</h5>
-                <button onclick='deleteBook(${bookItem.key})'>Remove</button>
-            </li>`;
-  const far = template.content.firstElementChild;
-  grab('books-container').appendChild(far);
-  document.forms['books-form'].reset();
+    localStorage.setItem("booksArray", JSON.stringify(oldBooks));
+    document.forms["books-form"].reset();
+    grab("books-container").innerHTML = "";
+    initialLoad();
+  }
 }
+const bookf = new Book();
 
 function initialLoad() {
-  let c = 0;
-  oldBooks.forEach((bookItem) => {
-    oldBooks[c].key = c;
-    template.innerHTML = `<li>
-                <h5>${bookItem.title} <span>by</span></h5>
-                <h5>${bookItem.author}</h5>
-                <button class='add-book-btn' onclick='deleteBook(${c})'>Remove</button>
-            </li>`;
+  oldBooks.forEach((bookItem, index) => {  
+  template.innerHTML = `<li><div class='book-details'>
+                <h5>${bookItem.title} by ${bookItem.author}</h5></div>
+                <button class='add-book-btn' onclick='bookf.deleteBook(${index})'>Remove</button>
+                </li>`;
     const far = template.content.firstElementChild;
-    grab('books-container').appendChild(far);
-    c += c + 1;
+    grab("books-container").appendChild(far);
   });
 }
 initialLoad();
